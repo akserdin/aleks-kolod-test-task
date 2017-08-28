4448863f89f1da70b81ef89011c172bfcb3c20f4
Dmytro Zolotar <dmytrozolotar@gmail.com>
2017-08-28 10:08:29 +0300

    Add unique index to title column

--- database/migrations/2017_08_25_151916_create_notes_table.php
+++ database/migrations/2017_08_25_151916_create_notes_table.php
@@ -16,7 +16,7 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('title');
+            $table->string('title')->unique();
             $table->text('body');
 
             $table->timestamps();
